84b1d7db6711a523b6034c178a76fd791d86fdb0
Somya Bhatt <somyacu1@gmail.com>
2023-07-11 14:32:38 +0530

    hmmm

---
+++ src/components/Todo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import Todoform from './Todoform';
+
+const Todo = () => {
+    return ( 
+        <div className='todo'>
+        <Todoform/>
+        </div>
+     );
+}
+ 
+export default Todo;
